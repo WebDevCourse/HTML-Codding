14a1fa40f740e0db418c97dda6b6f086a5a1ed48
matvii.kovtun <kovtun_m@ucu.edu.ua>
2018-03-08 15:52:58 +0200

    Added test slider

--- Matvii_Kovtun/slider/src/index.js
+++ Matvii_Kovtun/slider/src/index.js
@@ -6,14 +6,59 @@ let images = ["https://www.metaslider.com/wp-content/uploads/2014/11/mountains1.
 
 let currentImage = 1;
 
-buttonNext.addEventListener("click", () => {
-    currentImage = ++currentImage  % images.length;
+
+const image = (src) => new Promise((res, rej) => {
+    const image = new Image();
+    image.onload = res;
+    image.src = src;
+});
+
+
+const fadeOff = () => new Promise((res, rej) => {
+    const animate = () => {
+        if (!sliderImage.style.opacity) sliderImage.style.opacity = 1;
+        if (sliderImage.style.opacity <= 0) return res();
+        else sliderImage.style.opacity -= 0.01;
+        requestAnimationFrame(animate);
+    };
+    animate();
+});
+
+const fadeIn = () => new Promise((res, rej) => {
+    const animate = () => {
+        if (sliderImage.style.opacity >= 1) return res();
+        else sliderImage.style.opacity = 0.01 + Number(sliderImage.style.opacity);
+        requestAnimationFrame(animate);
+    };
+    animate();
+});
+
+const nextPic = async () => {
+    await image(images[currentImage]);
+    await fadeOff();
     sliderImage.src = images[currentImage];
+    await fadeIn();
+};
+
+
+const wait = (time) => new Promise((res, rej) => setTimeout(res, time));
+
+
+const run = async () => {
+    await wait(4000);
+    await nextPic();
+    run();
+};
+
+run();
+
+
+buttonNext.addEventListener("click", () => {
+    currentImage = ++currentImage % images.length;
+    nextPic();
 }, false);
 
 buttonPrev.addEventListener("click", () => {
     currentImage = (--currentImage + images.length) % images.length;
-    sliderImage.src = images[currentImage];
+    nextPic();
 }, false);
-
-
